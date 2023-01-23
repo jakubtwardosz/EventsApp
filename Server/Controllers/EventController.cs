f29b5f4a32e129e521005708fef319b2d4a771f1
Jakub Twardosz <jakubtwardosz@protonmail.com>
2023-01-23 10:28:31 +0100

    Write get all events method

---
+++ Server/Controllers/EventController.cs
@@ -0,0 +1,20 @@
+﻿using EventsApp.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventsApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Event>>> GetAllEvents()
+        {
+            var events = new List<Event> { 
+                new Event { Id = 1, Title = "Hello world", Description = "The first event" } };
+
+            return Ok(events);
+        }
+    }
+}
