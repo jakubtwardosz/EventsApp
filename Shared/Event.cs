98a1f67df7b2f2bd23dc921c51300e2842040119
Jakub Twardosz <jakubtwardosz@protonmail.com>
2023-01-23 10:13:33 +0100

    Create simply model

---
+++ Shared/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsApp.Shared
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        // Photo
+
+        // Start date
+        // End date
+
+        // Address
+        // Location
+        // Price
+
+        // Owner
+        // Category
+
+        // Phone
+        // Email
+    }
+}
