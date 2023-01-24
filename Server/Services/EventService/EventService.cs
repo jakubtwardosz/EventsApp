2b9771ab82bee081e49db79fa5ce745001bbf80f
Jakub Twardosz <jakubtwardosz@protonmail.com>
2023-01-24 13:13:42 +0100

    Add a repository pattern in EventService

--- Server/Services/EventService/EventService.cs
+++ Server/Services/EventService/EventService.cs
@@ -1,6 +1,32 @@
-﻿namespace EventsApp.Server.Services.EventService
+﻿using EventsApp.Shared;
+
+namespace EventsApp.Server.Services.EventService
 {
-    public class EventService
+    public class EventService : IEventService
     {
+        public List<Event> AddEvent(Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> DeleteEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> GetAllEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event GetSingleEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> UpdateEvent(int id, Event request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Server/Services/EventService/IEventService.cs
+++ Server/Services/EventService/IEventService.cs
@@ -1,6 +1,13 @@
-﻿namespace EventsApp.Server.Services.EventService
+﻿using EventsApp.Shared;
+
+namespace EventsApp.Server.Services.EventService
 {
     public interface IEventService
     {
+        List<Event> GetAllEvents();
+        Event GetSingleEvent(int id);
+        List<Event> AddEvent(Event newEvent);
+        List<Event> UpdateEvent(int id, Event request);
+        List<Event> DeleteEvent(int id);
     }
 }
