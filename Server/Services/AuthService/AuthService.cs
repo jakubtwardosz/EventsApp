282f2b89f538c33e8ceaf19976f862382c1d440e
Jakub Twardosz <jakubtwardosz@protonmail.com>
2023-01-25 13:31:04 +0100

    Implement user registration on the server

--- Server/Services/AuthService/AuthService.cs
+++ Server/Services/AuthService/AuthService.cs
@@ -1,15 +1,51 @@
-﻿namespace EventsApp.Server.Services.AuthService
+﻿using System.Security.Cryptography;
+
+namespace EventsApp.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        private readonly DataContext _context;
+
+        public AuthService(DataContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<bool> UserExists(string email)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            if(await UserExists(user.Email))
+            {
+                return new ServiceResponse<int> { Success = false, Message = "User already exists." };
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int> { Data = user.Id };
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            if (await _context.Users.AnyAsync(users => users.Email.ToLower()
+                .Equals(email.ToLower())))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
