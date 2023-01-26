9b04fe0b94c28451a3e5c38112d5b027cae32dbc
Jakub Twardosz <jakubtwardosz@protonmail.com>
2023-01-26 04:25:09 +0100

    Add BlazoredLocalStorage and implement login on the client

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,11 +5,14 @@ global using EventsApp.Client.Services.AuthService;
 using EventsApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IAuthService, AuthService>();
 
