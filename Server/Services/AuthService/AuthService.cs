cc18ae419d3b4d5cbfba3dc5e1fa2aa58d52a05b
Jakub Twardosz <jakubtwardosz@protonmail.com>
2023-01-26 02:28:35 +0100

    Prepare login on the server

--- Server/Services/AuthService/AuthService.cs
+++ Server/Services/AuthService/AuthService.cs
@@ -11,6 +11,16 @@ namespace EventsApp.Server.Services.AuthService
             _context = context;
         }
 
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>
+            {
+                Data = "token"
+            };
+
+            return response;
+        }
+
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
             if(await UserExists(user.Email))
