9130a5e3fc99773cb0a3490af0c61662bf3cc64e
Jakub Twardosz <jakubtwardosz@protonmail.com>
2023-03-09 00:40:29 +0100

    First working test

---
+++ TestProject/Systems/Controllers/TestEventController.cs
@@ -0,0 +1,43 @@
+﻿using EventsApp.Server.Services.EventService;
+using EventsApp.Server.Controllers;
+using EventsApp.Shared;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client.Extensions.Msal;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace UnitTests.Systems.Controllers
+{
+    public class TestEventController
+    {
+        [Fact]
+        public async Task Get_OnSuccess_InvokesEventServiceExaclyOnce()
+        {
+            // Arrange
+            var mockEventService = new Mock<IEventService>();
+            mockEventService
+                .Setup(service => service.GetEvents())
+                .Returns(Task.FromResult(
+                    new ServiceResponse<List<Event>>
+                    {
+                        Data = new List<Event>(),
+                        Success = true
+                    }));
+
+            var sut = new EventController(mockEventService.Object);
+
+            // Act
+
+            var result = await sut.GetEvents();
+
+            mockEventService.Verify(service => service.GetEvents(), Times.Once);
+        }
+    }
+}
